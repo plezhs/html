021f0b0023a53655d9a8f4ccfb48ec813e417db1
plezhs <pramasns@gmail.com>
2024-06-01 20:16:41 +0900

    change something about css

--- javascript.js
+++ javascript.js
@@ -10,22 +10,22 @@ function toggleList() {
 
 
 
-function showFunction(functionName) {
+function showFunction(functionName, wth) {
     const content = document.getElementById('functionContent');
     let htmlContent = '';
 
     switch (functionName) {
         case 'valshop':
-            htmlContent = `<object width="${innerWidth}" type="text/html" data="${functionName}.html"></object>`;
+            htmlContent = `<object width="${innerWidth-250}" type="text/html" data="${functionName}.html"></object>`;
             break;
         case 'nm':
-            htmlContent = `<object width="${innerWidth}" type="text/html" data="${functionName}.html"></object>`;
+            htmlContent = `<object width="${innerWidth-250}" type="text/html" data="${functionName}.html"></object>`;
             break;
         case 'function3':
-            htmlContent = `<object width="${innerWidth}" type="text/html" data="${functionName}.html"></object>`;
+            htmlContent = `<object width="${innerWidth-250}" type="text/html" data="${functionName}.html"></object>`;
             break;
         case 'function4':
-            htmlContent = `<object width="${innerWidth}" type="text/html" data="${functionName}.html"></object>`;
+            htmlContent = `<object width="${innerWidth-250}" type="text/html" data="${functionName}.html"></object>`;
             break;
         default:
             htmlContent = '<p>Select a function from the list to see its documentation.</p>';
