ee56299448c06f4d0290ebb12e6b277ac81c3aae
plezhs <124123128+plezhs@users.noreply.github.com>
2024-06-01 18:45:59 +0900

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,35 @@
+function toggleList() {
+    const list = document.getElementById('sidebar');
+    const buttonSection = document.getElementById('buttonSection');
+    const button = document.querySelector('.toggle-button');
+    list.classList.toggle('open');
+    buttonSection.classList.toggle('open');
+    buttonSection.classList.toggle('closed');
+    button.textContent = list.classList.contains('open') ? '<' : '>';
+}
+
+
+
+function showFunction(functionName) {
+    const content = document.getElementById('functionContent');
+    let htmlContent = '';
+
+    switch (functionName) {
+        case 'valshop':
+            htmlContent = `<object type="text/html" data="${functionName}.html"></object>`;
+            break;
+        case 'nm':
+            htmlContent = `<object type="text/html" data="${functionName}.html"></object>`;
+            break;
+        case 'function3':
+            htmlContent = `<object type="text/html" data="${functionName}.html"></object>`;
+            break;
+        case 'function4':
+            htmlContent = `<object type="text/html" data="${functionName}.html"></object>`;
+            break;
+        default:
+            htmlContent = '<p>Select a function from the list to see its documentation.</p>';
+    }
+
+    content.innerHTML = htmlContent;
+}
