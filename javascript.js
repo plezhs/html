6bfbcf03c5f62b3b7b627c0373c1a3ce72440906
plezhs <pramasns@gmail.com>
2024-06-05 12:52:04 +0900

    우히히ㅣ힣

--- javascript.js
+++ javascript.js
@@ -15,7 +15,7 @@ function showFunction(functionName) {
     let htmlContent = '';
     cmds = ['valshop', 'nm', 'set', 'info']
     if (cmds.includes(functionName)) {
-        htmlContent = `<object id="cmd" height="${window.innerHeight-100}" width="${window.innerWidth-500}" type="text/html" data="./cmddm/${functionName}.html"></object>`;
+        htmlContent = `<object id="cmd" height="${window.innerHeight}" width="${window.innerWidth-500}" type="text/html" data="./cmddm/${functionName}.html"></object>`;
     }else{
         htmlContent = '<p>Select a function from the list to see its documentation.</p>';
     }
