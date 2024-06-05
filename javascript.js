13edb209db4060fdf016d0c5ddb7dbbad2310123
plezhs <pramasns@gmail.com>
2024-06-05 12:19:34 +0900

    흐헤헿

--- javascript.js
+++ javascript.js
@@ -15,26 +15,10 @@ function showFunction(functionName) {
     let htmlContent = '';
     cmds = ['valshop', 'nm', 'set', 'info']
     if (cmds.includes(functionName)) {
-        htmlContent = `<object id="cmd" width="${window.innerWidth-500}" type="text/html" data="./cmddm/${functionName}.html"></object>`;
+        htmlContent = `<object id="cmd" height="${window.innerHeight-100}" width="${window.innerWidth-500}" type="text/html" data="./cmddm/${functionName}.html"></object>`;
     }else{
         htmlContent = '<p>Select a function from the list to see its documentation.</p>';
     }
-    // switch (functionName) {
-    //     case 'valshop':
-    //         htmlContent = `<object id="cmd" width="${window.innerWidth-500}" type="text/html" data="./cmddm/${functionName}.html"></object>`;
-    //         break;
-    //     case 'nm':
-    //         htmlContent = `<object id="cmd" width="${window.innerWidth-500}" type="text/html" data="./cmddm/${functionName}.html"></object>`;
-    //         break;
-    //     case 'set':
-    //         htmlContent = `<object id="cmd" width="${window.innerWidth-500}" type="text/html" data="./cmddm/${functionName}.html"></object>`;
-    //         break;
-    //     case 'info':
-    //         htmlContent = `<object id="cmd" width="${window.innerWidth-500}" type="text/html" data="./cmddm/${functionName}.html"></object>`;
-    //         break;
-    //     default:
-    //         htmlContent = '<p>Select a function from the list to see its documentation.</p>';
-    // }
 
     content.innerHTML = htmlContent;
 }
